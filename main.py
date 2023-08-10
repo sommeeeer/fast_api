efb23ec16e3e1d3e79ebcf8d2e71150584c0b4b5
magnus <magnus@dahleide.com>
2023-08-10 17:19:57 +0200

    Add static files folder.

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import fastapi
 import uvicorn
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates('templates')
-
+api.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates("templates")
 
 
 @api.get("/")
 def index(request: Request):
-    return templates.TemplateResponse('home/index.html', {'request': request})
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 
 if __name__ == "__main__":
     uvicorn.run(api, port=8000, host="127.0.0.1")
-    
