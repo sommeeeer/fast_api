a06d3433068cb38855052e47f98a8021b9552d1e
magnus <magnus@dahleide.com>
2023-08-10 17:08:52 +0200

    Add jinja2 template to index function

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import fastapi
 import uvicorn
-
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
+
 
 
 @api.get("/")
-def index():
-    return "Hello Weather app"
+def index(request: Request):
+    return templates.TemplateResponse('home/index.html', {'request': request})
 
 
 if __name__ == "__main__":
     uvicorn.run(api, port=8000, host="127.0.0.1")
+    
