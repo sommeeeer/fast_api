da5b52fda1e49c9798eeafb925506e58b5ab66b8
magnus <magnus@dahleide.com>
2023-08-13 16:12:51 +0200

    Add location model to weather endpoint.

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,9 +1,15 @@
-import fastapi
 from typing import Optional
+from fastapi import Depends
+import fastapi
+
+from models.location import Location
 
 router = fastapi.APIRouter()
 
 
-@router.get("/api/weather")
-def weather(state: Optional[str], country: str, units: str):
-    return "some weather"
+@router.get("/api/weather/{city}")
+def weather(
+    loc: Location = Depends(),
+    units: Optional[str] = "metric",
+):
+    return f"{loc.city} {loc.state} {loc.country} in {units}"
